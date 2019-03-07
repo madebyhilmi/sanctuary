3896f09aadb8911dbf50d0d3a8fc15cba721149d
Hilmi Abou-Saleh <2ndgenairborne@gmail.com>
2019-03-06 17:08:43 -0700

    Added main ViewControllers to Main Storyboard

---
+++ sanctuary/Controller/WelcomeScreenViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WelcomeScreenViewController.swift
+//  sanctuary
+//
+//  Created by Hilmi Abou-Saleh on 2019-03-06.
+//  Copyright © 2019 Project Sanctuary. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
