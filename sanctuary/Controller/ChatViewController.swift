36ba0be4fcff55a1d02827c0d5ebfb4eff2b34fa
s-seguin <stuart.seguin@ucalgary.ca>
2019-03-30 20:16:46 -0600

    basic sending of messages implemented

--- sanctuary/Controller/ChatViewController.swift
+++ sanctuary/Controller/ChatViewController.swift
@@ -11,8 +11,16 @@ import UIKit
 class ChatViewController: UITableViewController {
 
     fileprivate let cellId = "id"
+    
+    let textField : UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Don't be shy..."
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.borderStyle = .none
+        return textField
+    }()
 
-    let chatMessages = [
+    var chatMessages = [
         [ ChatMessage(text: "HEYEYEYEYEY", isIncoming: true, date: Date()),
           ChatMessage(text: "Hey cow brains, hows it going?", isIncoming: false, date: Date())
         ],
@@ -38,16 +46,64 @@ class ChatViewController: UITableViewController {
     }
     
     func setUpInputComponents() {
-        let containerView = UIView()
-        containerView.backgroundColor = .darkGray
-        containerView.translatesAutoresizingMaskIntoConstraints = false
+        let inputView = UIView()
+        inputView.backgroundColor = .white
+        inputView.translatesAutoresizingMaskIntoConstraints = false
         
-        navigationController?.view.addSubview(containerView)
+        navigationController?.view.addSubview(inputView)
         
-        containerView.leftAnchor.constraint(equalTo: (navigationController?.view.leftAnchor)!).isActive = true
-        containerView.bottomAnchor.constraint(equalTo: (navigationController?.view.bottomAnchor)!).isActive = true
-        containerView.widthAnchor.constraint(equalTo: (navigationController?.view.widthAnchor)!).isActive = true
-        containerView.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        inputView.leftAnchor.constraint(equalTo: (navigationController?.view.leftAnchor)!).isActive = true
+        inputView.bottomAnchor.constraint(equalTo: (navigationController?.view.bottomAnchor)!).isActive = true
+        inputView.widthAnchor.constraint(equalTo: (navigationController?.view.widthAnchor)!).isActive = true
+        inputView.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        inputView.addSubview(textField)
+        
+        textField.leftAnchor.constraint(equalTo: inputView.leftAnchor, constant: 10).isActive = true
+        textField.topAnchor.constraint(equalTo: inputView.topAnchor, constant: 5).isActive = true
+        textField.widthAnchor.constraint(equalTo: inputView.widthAnchor, constant: -83).isActive = true
+        
+        let sendBtn = UIButton(type: .system)
+        sendBtn.setTitle("Send", for: .normal)
+        sendBtn.translatesAutoresizingMaskIntoConstraints = false
+        
+        inputView.addSubview(sendBtn)
+        
+        sendBtn.rightAnchor.constraint(equalTo: inputView.rightAnchor).isActive = true
+        sendBtn.topAnchor.constraint(equalTo: inputView.topAnchor).isActive = true
+        sendBtn.widthAnchor.constraint(equalToConstant: 80).isActive = true
+        
+        sendBtn.addTarget(self, action: #selector(sendMessage), for: .touchUpInside)
+        
+        let seperatorLineView = UIView()
+        seperatorLineView.backgroundColor = UIColor(white: 0.90, alpha: 1)
+        seperatorLineView.translatesAutoresizingMaskIntoConstraints = false
+        inputView.addSubview(seperatorLineView)
+        
+        seperatorLineView.leftAnchor.constraint(equalTo: inputView.leftAnchor).isActive = true
+        seperatorLineView.topAnchor.constraint(equalTo: inputView.topAnchor).isActive = true
+        seperatorLineView.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        seperatorLineView.widthAnchor.constraint(equalTo: inputView.widthAnchor).isActive = true
+    }
+    
+    @objc func sendMessage() {
+        print(textField.text!)
+
+        var section = chatMessages.count - 1
+        var row = chatMessages[section].count - 1
+        
+        chatMessages[section].append(ChatMessage(text: textField.text!, isIncoming: false, date: Date()))
+        
+        //since we added an el get new values
+        section = chatMessages.count - 1
+        row = chatMessages[section].count - 1
+       
+        tableView.beginUpdates()
+        tableView.insertRows(at: [IndexPath.init(row: row, section: section)], with: .automatic)
+        tableView.endUpdates()
+        
+        tableView.scrollToRow(at: IndexPath.init(row: row, section: section), at: .bottom, animated: true)
+
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -84,16 +140,6 @@ class ChatViewController: UITableViewController {
         return 50
     }
     
-//    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-//        if let firstMessage = chatMessages[section].first {
-//            let dateFormatter = DateFormatter()
-//            dateFormatter.dateFormat = "YYYY/MM/dd h:mm:ss a"
-//
-//            return dateFormatter.string(from: firstMessage.date)
-//        }
-//
-//        return "Couldn't grab date"
-//    }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
