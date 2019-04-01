d124ffff465430cf250e3b82c996d00884a1a18d
Hilmi Abou-Saleh <2ndgenairborne@gmail.com>
2019-04-01 14:04:15 -0600

    Added support for interest in #7+ Realized issue with name validation exists

---
+++ sanctuary/Controller/FirstLaunch/RelationshipViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RelationshipViewController.swift
+//  sanctuary
+//
+//  Created by Hilmi Abou-Saleh on 2019-04-01.
+//  Copyright © 2019 Project Sanctuary. All rights reserved.
+//
+
+import UIKit
+
+class RelationshipViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
