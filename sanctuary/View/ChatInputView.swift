f87a00894bfe2a483473b8dfe9d14e0c3fb7a33c
s-seguin <stuart.seguin@ucalgary.ca>
2019-04-19 13:22:42 -0600

    chat - input fields now honour safe area

---
+++ sanctuary/View/ChatInputView.swift
@@ -0,0 +1,98 @@
+//
+//  ChatInputView.swift
+//  sanctuary
+//
+//  Created by Stuart Seguin on 2019-04-19.
+//  Copyright © 2019 Project Sanctuary. All rights reserved.
+//
+
+import UIKit
+
+class ChatInputView: UIView, UITextFieldDelegate {
+    
+    weak var chatLogController: ChatViewController? {
+        didSet {
+            sendButton.addTarget(chatLogController, action: #selector(ChatViewController.sendMessage), for: .touchUpInside)
+        }
+    }
+    
+    lazy var inputTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Don't be shy..."
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.delegate = self
+        return textField
+    }()
+    
+    let uploadImageView: UIImageView = {
+        let uploadImageView = UIImageView()
+        uploadImageView.isUserInteractionEnabled = true
+        uploadImageView.image = UIImage(named: "upload_image_icon")
+        uploadImageView.translatesAutoresizingMaskIntoConstraints = false
+        return uploadImageView
+    }()
+    
+    let sendButton = UIButton(type: .system)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .white
+        
+        addSubview(uploadImageView)
+        //x,y,w,h
+        uploadImageView.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
+        uploadImageView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        uploadImageView.widthAnchor.constraint(equalToConstant: 44).isActive = true
+        uploadImageView.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        
+        
+        sendButton.setTitle("Send", for: .normal)
+        sendButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        //what is handleSend?
+        
+        addSubview(sendButton)
+        //x,y,w,h
+        sendButton.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
+        sendButton.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        sendButton.widthAnchor.constraint(equalToConstant: 80).isActive = true
+        sendButton.heightAnchor.constraint(equalTo: heightAnchor).isActive = true
+        
+        addSubview(self.inputTextField)
+        //x,y,w,h
+        self.inputTextField.leftAnchor.constraint(equalTo: uploadImageView.rightAnchor, constant: 8).isActive = true
+        self.inputTextField.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        self.inputTextField.rightAnchor.constraint(equalTo: sendButton.leftAnchor).isActive = true
+        self.inputTextField.heightAnchor.constraint(equalTo: heightAnchor).isActive = true
+        
+        let separatorLineView = UIView()
+        separatorLineView.backgroundColor = UIColor(white: 0.90, alpha: 1)
+        separatorLineView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(separatorLineView)
+        //x,y,w,h
+        separatorLineView.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
+        separatorLineView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        separatorLineView.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
+        separatorLineView.heightAnchor.constraint(equalToConstant: 1).isActive = true
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        chatLogController?.sendMessage()
+        return true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func didMoveToWindow() {
+        super.didMoveToWindow()
+        if #available(iOS 11.0, *) {
+            if let window = self.window {
+                self.bottomAnchor.constraint(lessThanOrEqualToSystemSpacingBelow: window.safeAreaLayoutGuide.bottomAnchor, multiplier: 1.0).isActive = true
+            }
+        }
+    }
+    
+}
